e36636393216cfbe100fb4bfc2aed32cf8f6ed57
amanbaid99 <amanbaid99@gmail.com>
2020-04-01 11:11:55 +0530

    initial commmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{Title,Meta}from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyPortfolio';
+
+  
+  constructor(private titleService:Title, private meta:Meta){
+    titleService.setTitle('My Personal Blog')
+    
+    meta.updateTag({name:'viewport',content:'width=device-width, initial-scale=1, shrink-to-fit=no'});
+    meta.addTag({name:'description',content:'This is My Personal Blog'},true);
+    
+    meta.addTag({name:'author',content:'Aman Baid'},true);
+}
 }
